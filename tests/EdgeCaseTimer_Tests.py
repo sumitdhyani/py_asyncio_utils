68b42ab38ba7a73cce459fd316583faa2195f7d7
sumitdhyani <sumitdhyani1@gmail.com>
2025-12-30 19:41:50 +0530

    Added the test for overrun behavior

--- tests/EdgeCaseTimer_Tests.py
+++ tests/EdgeCaseTimer_Tests.py
@@ -11,10 +11,10 @@ from Timer import Timer
 
 
 async def startAndStopTimer(timer: Timer, sleepInterval: int) -> int:
-    await timer.start()
+    timer.start()
     # Sleep for the specified interval
     await asyncio.sleep(sleepInterval)
-    await timer.stop()
+    timer.stop()
 
 
 class TimerTests(unittest.IsolatedAsyncioTestCase):
@@ -35,9 +35,34 @@ class TimerTests(unittest.IsolatedAsyncioTestCase):
                 timeoutInSecs, totalTestDurationInSecs, expectedTics, datetime.now()
             )
         )
-        await timer.start()
+        timer.start()
         await asyncio.sleep(totalTestDurationInSecs - timeoutInSecs)
-        await timer.stop()
+        timer.stop()
         await asyncio.sleep(timeoutInSecs)
+
+        self.assertEqual(counter, expectedTics)
+
+    async def test_timer_starts_and_callback_overruns(self):
+        timeoutInSecs: int = 1
+        totalTestDurationInSecs: int = 8
+        expectedTics: int = (totalTestDurationInSecs // timeoutInSecs) // 2
+        counter: int = 0
+
+        async def increment_counter():
+            print(datetime.now())
+            # Simulate a long-running task that overruns the next tick
+            nonlocal counter
+            counter += 1
+            await asyncio.sleep(1.5)
+
+        timer: Timer = Timer(timeoutInSecs * 1_000_000_000, increment_counter)
+        print(
+            "Starting timer test: timeout={}s, total duration={}s, expected tics={}, time={}".format(
+                timeoutInSecs, totalTestDurationInSecs, expectedTics, datetime.now()
+            )
+        )
+        timer.start()
+        await asyncio.sleep(totalTestDurationInSecs)
+        timer.stop()
 
         self.assertEqual(counter, expectedTics)
